e844a0f3c5c29727d62f00fec93b76a64a1f683b
illy1990 <jascha.hell@outlook.de>
2020-03-27 16:53:30 +0100

    Color bug fixxed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
-let RandomColorToGuess1;
-let RandomColorToGuess2;
-let RandomColorToGuess3;
+let RandomColor1;
+let RandomColor2;
+let RandomColor3;
 let RandomColor4;
 let RandomColor5;
 let RandomColor6;
@@ -16,15 +16,12 @@ let RandomColor15;
 
 // --------------------------------------- Farbe 1 ---------------------------------------
 function ColorOutput1() {
-    RandomColorToGuess1 = Math.floor(Math.random() * 256);
-    RandomColorToGuess2 = Math.floor(Math.random() * 256);
-    RandomColorToGuess3 = Math.floor(Math.random() * 256);
-    let ColorOutput1 = `rgb(${RandomColorToGuess1}, ${RandomColorToGuess2}, ${RandomColorToGuess3})`;
-    // document.getElementById("Output1").style.backgroundColor = ColorOutput1;
-    return ColorOutput1;
+    RandomColor1 = Math.floor(Math.random() * 256);
+    RandomColor2 = Math.floor(Math.random() * 256);
+    RandomColor3 = Math.floor(Math.random() * 256);
+    let ColorOutput1 = `rgb(${RandomColor1}, ${RandomColor2}, ${RandomColor3})`;
+    document.getElementById("Output1").style.backgroundColor = ColorOutput1;
 }
-
-ColorOutput1()
 // --------------------------------------- Farbe 1 ---------------------------------------
 // --------------------------------------- Farbe 2 ---------------------------------------
 function ColorOutput2() {
@@ -32,11 +29,8 @@ function ColorOutput2() {
     RandomColor5 = Math.floor(Math.random() * 256);
     RandomColor6 = Math.floor(Math.random() * 256);
     let ColorOutput2 = `rgb(${RandomColor4}, ${RandomColor5}, ${RandomColor6})`;
-    // document.getElementById("Output1").style.backgroundColor = ColorOutput1;
-    return ColorOutput2;
+    document.getElementById("Output2").style.backgroundColor = ColorOutput2;
 }
-
-ColorOutput2()
 // --------------------------------------- Farbe 2 ---------------------------------------
 // --------------------------------------- Farbe 3 ---------------------------------------
 function ColorOutput3() {
@@ -44,11 +38,8 @@ function ColorOutput3() {
     RandomColor8 = Math.floor(Math.random() * 256);
     RandomColor9 = Math.floor(Math.random() * 256);
     let ColorOutput3 = `rgb(${RandomColor7}, ${RandomColor8}, ${RandomColor9})`;
-    // document.getElementById("Output1").style.backgroundColor = ColorOutput1;
-    return ColorOutput3;
+    document.getElementById("Output3").style.backgroundColor = ColorOutput3;
 }
-
-ColorOutput3()
 // --------------------------------------- Farbe 3 ---------------------------------------
 // --------------------------------------- Farbe 4 ---------------------------------------
 function ColorOutput4() {
@@ -56,11 +47,8 @@ function ColorOutput4() {
     RandomColor11 = Math.floor(Math.random() * 256);
     RandomColor12 = Math.floor(Math.random() * 256);
     let ColorOutput4 = `rgb(${RandomColor10}, ${RandomColor11}, ${RandomColor12})`;
-    // document.getElementById("Output1").style.backgroundColor = ColorOutput1;
-    return ColorOutput4;
+    document.getElementById("Output4").style.backgroundColor = ColorOutput4;
 }
-
-ColorOutput4()
 // --------------------------------------- Farbe 4 ---------------------------------------
 // --------------------------------------- Farbe 5 ---------------------------------------
 function ColorOutput5() {
@@ -68,9 +56,16 @@ function ColorOutput5() {
     RandomColor14 = Math.floor(Math.random() * 256);
     RandomColor15 = Math.floor(Math.random() * 256);
     let ColorOutput5 = `rgb(${RandomColor13}, ${RandomColor14}, ${RandomColor15})`;
-    // document.getElementById("Output1").style.backgroundColor = ColorOutput1;
-    return ColorOutput5;
+    document.getElementById("Output5").style.backgroundColor = ColorOutput5;
 }
-
-ColorOutput5()
 // --------------------------------------- Farbe 5 ---------------------------------------
+
+ColorOutput1()
+ColorOutput2()
+ColorOutput3()
+ColorOutput4()
+ColorOutput5()
+
+document.getElementById("RestartButon").addEventListener("click", () => {
+    location.reload()
+})
