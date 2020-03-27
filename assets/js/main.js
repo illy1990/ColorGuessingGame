4649905bc37c917ec899dd2fd56149c40dc7db04
illy1990 <jascha.hell@outlook.de>
2020-03-27 14:10:30 +0100

    Funktionen stehen

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,18 @@
 let RandomColorToGuess1;
 let RandomColorToGuess2;
 let RandomColorToGuess3;
-let RandomColor4 = Math.floor(Math.random() * 256)
-let RandomColor5 = Math.floor(Math.random() * 256)
-let RandomColor6 = Math.floor(Math.random() * 256)
-let RandomColor7 = Math.floor(Math.random() * 256)
-let RandomColor8 = Math.floor(Math.random() * 256)
-let RandomColor9 = Math.floor(Math.random() * 256)
-
-
+let RandomColor4;
+let RandomColor5;
+let RandomColor6;
+let RandomColor7;
+let RandomColor8;
+let RandomColor9;
+let RandomColor10;
+let RandomColor11;
+let RandomColor12;
+let RandomColor13;
+let RandomColor14;
+let RandomColor15;
 
 // --------------------------------------- Farbe 1 ---------------------------------------
 function ColorOutput1() {
@@ -16,14 +20,57 @@ function ColorOutput1() {
     RandomColorToGuess2 = Math.floor(Math.random() * 256);
     RandomColorToGuess3 = Math.floor(Math.random() * 256);
     let ColorOutput1 = `rgb(${RandomColorToGuess1}, ${RandomColorToGuess2}, ${RandomColorToGuess3})`;
-    document.getElementById("Output1").style.backgroundColor = ColorOutput1;
+    // document.getElementById("Output1").style.backgroundColor = ColorOutput1;
     return ColorOutput1;
 }
 
 ColorOutput1()
 // --------------------------------------- Farbe 1 ---------------------------------------
 // --------------------------------------- Farbe 2 ---------------------------------------
+function ColorOutput2() {
+    RandomColor4 = Math.floor(Math.random() * 256);
+    RandomColor5 = Math.floor(Math.random() * 256);
+    RandomColor6 = Math.floor(Math.random() * 256);
+    let ColorOutput2 = `rgb(${RandomColor4}, ${RandomColor5}, ${RandomColor6})`;
+    // document.getElementById("Output1").style.backgroundColor = ColorOutput1;
+    return ColorOutput2;
+}
 
-
-
+ColorOutput2()
 // --------------------------------------- Farbe 2 ---------------------------------------
+// --------------------------------------- Farbe 3 ---------------------------------------
+function ColorOutput3() {
+    RandomColor7 = Math.floor(Math.random() * 256);
+    RandomColor8 = Math.floor(Math.random() * 256);
+    RandomColor9 = Math.floor(Math.random() * 256);
+    let ColorOutput3 = `rgb(${RandomColor7}, ${RandomColor8}, ${RandomColor9})`;
+    // document.getElementById("Output1").style.backgroundColor = ColorOutput1;
+    return ColorOutput3;
+}
+
+ColorOutput3()
+// --------------------------------------- Farbe 3 ---------------------------------------
+// --------------------------------------- Farbe 4 ---------------------------------------
+function ColorOutput4() {
+    RandomColor10 = Math.floor(Math.random() * 256);
+    RandomColor11 = Math.floor(Math.random() * 256);
+    RandomColor12 = Math.floor(Math.random() * 256);
+    let ColorOutput4 = `rgb(${RandomColor10}, ${RandomColor11}, ${RandomColor12})`;
+    // document.getElementById("Output1").style.backgroundColor = ColorOutput1;
+    return ColorOutput4;
+}
+
+ColorOutput4()
+// --------------------------------------- Farbe 4 ---------------------------------------
+// --------------------------------------- Farbe 5 ---------------------------------------
+function ColorOutput5() {
+    RandomColor13 = Math.floor(Math.random() * 256);
+    RandomColor14 = Math.floor(Math.random() * 256);
+    RandomColor15 = Math.floor(Math.random() * 256);
+    let ColorOutput5 = `rgb(${RandomColor13}, ${RandomColor14}, ${RandomColor15})`;
+    // document.getElementById("Output1").style.backgroundColor = ColorOutput1;
+    return ColorOutput5;
+}
+
+ColorOutput5()
+// --------------------------------------- Farbe 5 ---------------------------------------
